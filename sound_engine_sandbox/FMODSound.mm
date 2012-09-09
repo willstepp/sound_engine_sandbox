da448e4e3de6854527c9249ed24f606f20181c09
Daniel Stepp <willstepp@gmail.com>
2012-09-08 22:19:37 -0400

    added effect parameter mappings

--- sound_engine_sandbox/FMODSound.mm
+++ sound_engine_sandbox/FMODSound.mm
@@ -25,6 +25,7 @@
 -(void)ensureSoundReleased;
 -(FMOD::DSP*)getDSPWithEffectType:(EffectType)et;
 -(FMOD_DSP_TYPE)getDSPTypeWithEffectType:(EffectType)et;
+-(int)getDSPParameterWithEffectParameter:(EffectParameter)ep;
 @end
 
 @implementation FMODSound
@@ -87,6 +88,76 @@
             return FMOD_DSP_TYPE_DISTORTION;
         case EffectType::Flange:
             return FMOD_DSP_TYPE_FLANGE;
+        default:
+            return FMOD_DSP_TYPE_UNKNOWN;
+    }
+}
+
+-(int)getDSPParameterWithEffectParameter:(EffectParameter)ep
+{
+    switch (ep)
+    {
+        case EffectParameter::Reverb_DryLevel:
+            return FMOD_DSP_SFXREVERB_DRYLEVEL;
+        case EffectParameter::Reverb_Room:
+            return FMOD_DSP_SFXREVERB_ROOM;
+        case EffectParameter::Reverb_RoomHF:
+            return FMOD_DSP_SFXREVERB_ROOMHF;
+        case EffectParameter::Reverb_DecayTime:
+            return FMOD_DSP_SFXREVERB_DECAYTIME;
+        case EffectParameter::Reverb_DecayHFRatio:
+            return FMOD_DSP_SFXREVERB_DECAYHFRATIO;
+        case EffectParameter::Reverb_ReflectionsLevel:
+            return FMOD_DSP_SFXREVERB_REFLECTIONSLEVEL;
+        case EffectParameter::Reverb_ReflectionsDelay:
+            return FMOD_DSP_SFXREVERB_REFLECTIONSDELAY;
+        case EffectParameter::Reverb_ReverbLevel:
+            return FMOD_DSP_SFXREVERB_REVERBLEVEL;
+        case EffectParameter::Reverb_ReverbDelay:
+            return FMOD_DSP_SFXREVERB_REVERBDELAY;
+        case EffectParameter::Reverb_Diffusion:
+            return FMOD_DSP_SFXREVERB_DIFFUSION;
+        case EffectParameter::Reverb_Density:
+            return FMOD_DSP_SFXREVERB_DENSITY;
+        case EffectParameter::Reverb_HFReference:
+            return FMOD_DSP_SFXREVERB_HFREFERENCE;
+        case EffectParameter::Reverb_RoomLF:
+            return FMOD_DSP_SFXREVERB_ROOMLF;
+        case EffectParameter::Reverb_LFReference:
+            return FMOD_DSP_SFXREVERB_LFREFERENCE;
+            
+        case EffectParameter::Echo_Delay:
+            return FMOD_DSP_ECHO_DELAY;
+        case EffectParameter::Echo_DecayRatio:
+            return FMOD_DSP_ECHO_DECAYRATIO;
+        case EffectParameter::Echo_MaxChannels:
+            return FMOD_DSP_ECHO_MAXCHANNELS;
+        case EffectParameter::Echo_DryMix:
+            return FMOD_DSP_ECHO_DRYMIX;
+        case EffectParameter::Echo_WetMix:
+            return FMOD_DSP_ECHO_WETMIX;
+        
+        case EffectParameter::Flange_DryMix:
+            return FMOD_DSP_FLANGE_DRYMIX;
+        case EffectParameter::Flange_WetMix:
+            return FMOD_DSP_FLANGE_WETMIX;
+        case EffectParameter::Flange_Depth:
+            return FMOD_DSP_FLANGE_DEPTH;
+        case EffectParameter::Flange_Rate:
+            return FMOD_DSP_FLANGE_RATE;
+            
+        case EffectParameter::Distortion_Level:
+            return FMOD_DSP_DISTORTION_LEVEL;
+            
+        case EffectParameter::Pitch_Value:
+            return FMOD_DSP_PITCHSHIFT_PITCH;
+        case EffectParameter::Pitch_FFTSize:
+            return FMOD_DSP_PITCHSHIFT_FFTSIZE;
+        case EffectParameter::Pitch_Overlap:
+            return FMOD_DSP_PITCHSHIFT_OVERLAP;
+        case EffectParameter::Pitch_MaxChannels:
+            return FMOD_DSP_PITCHSHIFT_MAXCHANNELS;
+            
         default:
             return FMOD_DSP_TYPE_UNKNOWN;
     }
@@ -160,6 +231,9 @@
 
 -(void)setEffectValueForType:(EffectType)et forParameter:(EffectParameter)ep withValue:(float)value
 {
+    FMOD::DSP * effect = [self getDSPWithEffectType:et];
+    if (effect)
+        effect->setParameter([self getDSPParameterWithEffectParameter:ep], value);
 }
 
 @end
